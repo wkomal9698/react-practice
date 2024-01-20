dd74f204a854294ebc6a9558aa46ea5d37719ff3
Komal <wkomal9698@gmail.com>
2024-01-20 19:22:30 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1", {id:"heading1", xyz:"abc"}, "Hello World from React!1");
+const heading2 = React.createElement("h1", {id:"heading2", xyz:"abc"}, "Hello World from React!2");
+const parent = React.createElement("div", {"id": "parent"}, [React.createElement("div", {id:"child"}, [heading1, heading2]), heading1, heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
